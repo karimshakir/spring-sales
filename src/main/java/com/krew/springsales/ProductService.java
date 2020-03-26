aca66ae65e2f87f5261fc1a2f4d81e38326b36d9
“karimshakir” <karimshakir99@gmail.com>
2020-03-25 23:02:26 -0500

    complete repo

--- src/main/java/com/krew/springsales/ProductService.java
+++ src/main/java/com/krew/springsales/ProductService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -11,5 +12,18 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repo;
+
+    public List<Product> listAll(){
+        return repo.findAll();
+    }
+    public void save (Product product) {
+        repo.save(product);
+    }
+    public Product get (Integer id) {
+        return repo.findById(id).get();
+    }
+    public void delete (Integer id) {
+        repo.deleteById(id);
+    }
 
 }
